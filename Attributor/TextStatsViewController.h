50025607ec37c9a251ebd7193f87c3e9f3086cac
NSCoolMan <251758871@qq.com>
2016-02-04 00:31:33 +0800

    Add UINavigationController to Attributor
    1.加入UINavigationController，学习其用法，重点注意segue的相关用法，重要属性：标志符identifier和desti
    nationViewController等。
    2.继续巩固视图控制器的生命周期，如viewDidLoad、viewWillAppear:、prepareForSegue:sender:（介于
    老视图将要消失和新视图将要产生中间）等方法。
    3.继续学习NSAttributedString的相关用法。

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by 天桥雨 on 16/2/3.
+//  Copyright (c) 2016年 Coolman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
